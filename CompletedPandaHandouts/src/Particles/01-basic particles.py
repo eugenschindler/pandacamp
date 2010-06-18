aa148f5c556745667cdaa6e6e55a244a62921a3f
michael.reed33 <michael.reed33@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-18 22:53:37 +0000

    messing with basic particles

--- CompletedPandaHandouts/src/Particles/01-basic particles.py
+++ CompletedPandaHandouts/src/Particles/01-basic particles.py
@@ -5,14 +5,19 @@ from Panda import *
 world.color = black
 #intervalRings(position = P3(0,0,0))
 #likeFountainWater(position = P3(0,0,0))
-#shakenSparkles(position = P3(0,0,0))
-warpSpeed(position = P3(0,10,0))
+#shakenSparkles(position = P3(0,0,0), color=yellow, endColor=blue)
+#warpSpeed(position = P3(0,10,0))
 #heavySnow(position = P3(0,0,0))
 #lightSnow(position = P3(0,0,0))
 #p = explosion(position = P3(0,0,0))
 #fw = fireWork(position = P3(0,0,0))
 #ex = explosion(position = P3(-1,0,0) )
-#fireish(position = P3(0,0,0))
+
+f = fireish(position = P3(0,0,1), texture="myfire.png")
+def shutup(m, v):
+    m.stop()
+f.react1(localTimeIs(1), shutup)
+
 
 # Run each of these and see what they do
 
