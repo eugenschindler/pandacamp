8d50696bda74badb4342fc017a6f93287af6cc49
ProfessorJohnPeterson@gmail.com <ProfessorJohnPeterson@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-18 05:55:10 +0000

    Changed handling of paths in models.  Renamed some models to use camelcase and deleted some models that don't work well.
    This will break code!

--- pandacamp/src/Models.py
+++ pandacamp/src/Models.py
@@ -23,13 +23,13 @@ import os, sys
 ## Why is this pathname different???  -- jcp
 #pandas
 def panda(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(fileName = "models/panda-model", name = 'Panda',
+    return modelHandle(fileName = "panda-model.egg.pz", name = 'Panda',
                        localSize = .002*.91, localPosition = P3(0,.21,0),
                        localOrientation = HPR(0,0,0), **a)
 
 #jointed models
 def ralph(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/Ralph/ralph.egg", name = 'Ralph',
+    return modelHandle("Ralph/ralph.egg", name = 'Ralph',
                        localSize = .18,
                        joints = [('neck', 'Neck'), ('leftWrist', 'LeftWrist'),
                                  ('rightWrist', 'RightWrist'),
@@ -38,7 +38,7 @@ def ralph(**a):#Works as of 6-23-08 ~ Kendric
                                  ('rightKnee', 'RightKnee')], animations = {"walk" : g.pandaPath + "/models/Ralph/ralph-walk.egg"}, frame = 4,  **a )
 
 def sonic(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/sonic/sonic", name = "Sonic",
+    return modelHandle("sonic/sonic.egg", name = "Sonic",
                        localSize = 0.036,  localOrientation = HPR(-3.14159,   0.66,   0.00),
                        joints = [('neck', 'Neck'), ('leftEyeBrow', 'LeftEyeBrow'), ('rightEyeBrow', 'RightEyeBrow'),
                                  ('leftLowerSpike', 'LeftLowerSpike'), ('lowerRightSpike', 'LowerRightSpike'),
@@ -53,36 +53,31 @@ def sonic(**a):#Works as of 6-23-08 ~ Kendric
                                  ('leftAnkle', 'LeftAnkle'), ('rightAnkle', 'RightAnkle'), ], animations = {"walk" : g.pandaPath + "/models/sonic/sonic-run.egg"}, frame = 11, **a)
 
 
-#misc objects
-def sphere(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/sphere/sphere", name = 'Sphere', **a)
-
 
 def sphere(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/sphere/sphere", name = 'Sphere', localSize = .25, **a)
-
+    return modelHandle("sphere/sphere.egg", name = 'Sphere', localSize = .25, **a)
 
 
 def soccerball(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/soccerball/soccerball", name = "SoccerBall",
+    return modelHandle("soccerball/soccerball.egg", name = "SoccerBall",
                     localSize = 1.86842024326, localPosition = P3(0.0, 0.0, -0.0877192616463), localOrientation = HPR(0.0, 0, 0), **a)
 
 
 def volleyball(**a):#Works as of 6-23-08 ~ Kendric
     #Formerly volleyBall. -Alexandra
-    return modelHandle(g.pandaPath + "/models/volleyBall/volleyball", name = "VolleyBall",
+    return modelHandle("volleyBall/volleyball.egg", name = "VolleyBall",
                     localSize = 1.8, **a)
 
 
 
 def stretcher(**a):
-    return modelHandle(g.pandaPath + "/models/stretcher/strecher",
+    return modelHandle("stretcher/strecher.egg",
                        name = "Stretcher", localPosition = P3(0, 0, -.648),
                        localSize = .0163, **a)
 
 def chair(**a):
     #Formerly deskChair. -Alexandra
-    return modelHandle(g.pandaPath + "/models/deskChair/deskchair", name = "DeskChair",
+    return modelHandle("deskChair/deskchair.egg", name = "DeskChair",
                        localSize = .0011579, localPosition = P3(0, 0, -.455),
                        localOrientation = HPR(3.09, 0, 0), **a)
 
@@ -90,89 +85,89 @@ def chair(**a):
 #characters/Creatures
 
 def gorilla(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/gorilla/gorilla", name = "Gorilla",
+    return modelHandle("gorilla/gorilla.egg", name = "Gorilla",
                        localSize = .123, localOrientation = HPR(0, -.05, 0),
                        localPosition = P3(0,0,0), **a)
 
 def bunny(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/bunny/bunny", name = "Bunny",
+    return modelHandle("bunny/bunny.egg", name = "Bunny",
                        localSize = .195, **a)
 
 def boyBalloon(**a):#Works as of 6-23-08 ~ Kendric - terrifying
-    return modelHandle(g.pandaPath + "/models/boyballoon/boymodel",
+    return modelHandle("boyballoon/boymodel.egg",
                        name = "Boy", localSize = .00145, localOrientation = HPR(0, 0, 0),
                        localPosition = P3(0, 0, -.17),
                        **a)
 
 
 def r2d2(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/r2d2/r2d2", name = "R2d2",
+    return modelHandle("r2d2/r2d2.egg", name = "R2d2",
                        localSize = .38, localOrientation = HPR(-3.09,0,0),
                         **a)
 
 def girl(**a):
     #Formerly eve. -Alexandra
-    return modelHandle(g.pandaPath + "/models/eve/eve", name = "Eve",
+    return modelHandle("eve/eve.egg", name = "Eve",
                        localSize = .1757, localPosition = P3(0, 0, -.043),
                         **a)
 
-def monster(**a):
-    return modelHandle(g.pandaPath + "/models/monster/monster1", name = "monster1",
-                       localSize = 0.3921, localPosition = P3(0, 0, 0),
-                       localOrientation = HPR(0, 0, 0),
-                       joints = [#('rootDummy', 'RootDummy'), ('root', 'Root'),
-                                 ('rightShoulder', 'R_Shoulder'), ('rightElbow', 'R_Elbow'),
-                                 ('rightWrist', 'R_Wrist'), ('rightClaw', 'R_Claw'),
-                                 ('leftShoulder', 'L_Shoulder'), ('leftElbow', 'L_Elbow'),
-                                 ('leftWrist', 'L_Wrist'), ('leftClaw', 'L_Claw'),
-#                                 ('effector1', 'effector1'), ('effector2', 'effector2'), ('effector3', 'effector3'),
-#                                 ('effector4', 'effector4'),('effector5', 'effector5'), ('effector6', 'effector6'),
-                                 ('lidRoot', 'LidRoot'), ('lid', 'Lid'),
-                                 ('tentacle1Root', 'Tentacle1_Root'), ('tentacle1Mid', 'Tentacle1_Mid'), ('tentacle1Tip', 'Tentacle1_Tip'),
-                                 ('tentacle2Root', 'Tentacle2_Root'), ('tentacle2Mid', 'Tentacle2_Mid'), ('tentacle2Tip', 'Tentacle2_Tip'),
-                                 ('tentacle3Root', 'Tentacle3_Root'), ('tentacle3Mid', 'Tentacle3_Mid'), ('tentacle3Tip', 'Tentacle3_Tip'),
-                                 ('tentacle4Root', 'Tentacle4_Root'), ('tentacle4Mid', 'Tentacle4_Mid'), ('tentacle4Tip', 'Tentacle4_Tip'),
-#                                 ('nurbsCircle1', 'nurbsCircle1'), ('nurbsCircle2', 'nurbsCircle2'), ('nurbsCircle3', 'nurbsCircle3'),
-#                                 ('nurbsCircle4', 'nurbsCircle4'), ('nurbsCircle5', 'nurbsCircle5'), ('nurbsCircle6', 'nurbsCircle6')
-                                 ],
-                                 animations = {"explode" : g.pandaPath + "/models/monster/monster1-explode.egg"}, frame = 3, **a)
+#def monster(**a):
+#    return modelHandle(g.pandaPath + "/models/monster/monster1", name = "monster1",
+#                       localSize = 0.3921, localPosition = P3(0, 0, 0),
+#                       localOrientation = HPR(0, 0, 0),
+#                       joints = [#('rootDummy', 'RootDummy'), ('root', 'Root'),
+#                                 ('rightShoulder', 'R_Shoulder'), ('rightElbow', 'R_Elbow'),
+#                                 ('rightWrist', 'R_Wrist'), ('rightClaw', 'R_Claw'),
+#                                 ('leftShoulder', 'L_Shoulder'), ('leftElbow', 'L_Elbow'),
+#                                 ('leftWrist', 'L_Wrist'), ('leftClaw', 'L_Claw'),
+##                                 ('effector1', 'effector1'), ('effector2', 'effector2'), ('effector3', 'effector3'),
+##                                 ('effector4', 'effector4'),('effector5', 'effector5'), ('effector6', 'effector6'),
+#                                 ('lidRoot', 'LidRoot'), ('lid', 'Lid'),
+#                                 ('tentacle1Root', 'Tentacle1_Root'), ('tentacle1Mid', 'Tentacle1_Mid'), ('tentacle1Tip', 'Tentacle1_Tip'),
+#                                 ('tentacle2Root', 'Tentacle2_Root'), ('tentacle2Mid', 'Tentacle2_Mid'), ('tentacle2Tip', 'Tentacle2_Tip'),
+#                                 ('tentacle3Root', 'Tentacle3_Root'), ('tentacle3Mid', 'Tentacle3_Mid'), ('tentacle3Tip', 'Tentacle3_Tip'),
+#                                 ('tentacle4Root', 'Tentacle4_Root'), ('tentacle4Mid', 'Tentacle4_Mid'), ('tentacle4Tip', 'Tentacle4_Tip'),
+##                                 ('nurbsCircle1', 'nurbsCircle1'), ('nurbsCircle2', 'nurbsCircle2'), ('nurbsCircle3', 'nurbsCircle3'),
+##                                 ('nurbsCircle4', 'nurbsCircle4'), ('nurbsCircle5', 'nurbsCircle5'), ('nurbsCircle6', 'nurbsCircle6')
+#                                 ],
+#                                 animations = {"explode" : g.pandaPath + "/models/monster/monster1-explode.egg"}, frame = 3, **a)
 
 def tails(**a):
-    return modelHandle(g.pandaPath + "/models/tails/tails", name = "tails",
+    return modelHandle("tails/tails.egg", name = "tails",
                        localSize = 0.03368, localPosition = P3(0, 0, -.07),
                        localOrientation = HPR(-3.14159, 0, 0), **a)
 
 
 
  #Vehicles
-def crudeplane(**a):#Have not tested - Matt
-    return modelHandle(g.pandaPath + "/models/crude_plane", name = "Crudeplane",
-                    localSize = 2, **a)
+#def crudeplane(**a):#Have not tested - Matt
+#    return modelHandle("crude_plane.egg", name = "Crudeplane",
+#                    localSize = 2, **a)
 
 def truck(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/truck/cartruck", name = "Truck",
+    return modelHandle("truck/cartruck.egg", name = "Truck",
                     localPosition = P3(0, 0, -.01), localSize = .44, **a)
 
 def ford(**a):#Works as of 6-23-08 ~ Kendric - has error message, but seems to work fine regardless.
     #Formerly fordCar. -Alexandra
-    return modelHandle(g.pandaPath + "/models/fordCar/ford", name = "FordCar",
+    return modelHandle("fordCar/ford.egg", name = "FordCar",
                       localSize = .14, localPosition = P3(.38, -.63, 0),
                       localOrientation = HPR(0, 0, 0),
                       **a)
 
 def jeep(**a):#Works as of 6-23-08 ~ Kendric
     #Error about window texture, but works. -Alexandra
-    return modelHandle(g.pandaPath + "/models/jeep/jeep", name = "Jeep",
+    return modelHandle("jeep/jeep.egg", name = "Jeep",
                        localSize = .1, **a)
 
 def boeing707(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/boeing707/boeing707",
+    return modelHandle("boeing707/boeing707.egg",
                        name = "Boeing707", localSize = .02, localOrientation = HPR(3.14159, 0, 0),
                        localPosition = P3(0, 0, 0),
                        **a)
 
-def hangglider(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/hangglider/hang-glider-1",
+def hangGlider(**a):#Works as of 6-23-08 ~ Kendric
+    return modelHandle("hangglider/hang-glider-1.egg",
                        name = "Hangglider",
                        localPosition = P3(0, 0, 0.175), localOrientation = HPR(0, 3.14159, 3.14159),
                        localSize = .0654,
@@ -182,33 +177,34 @@ def hangglider(**a):#Works as of 6-23-08 ~ Kendric
 #Buildings/Scenery
 def russianBuilding(**a):
     #Formerly russianBuilding. -Alexandra
-    return modelHandle(g.pandaPath + "/models/russianBuilding/tetris-building",
+    return modelHandle("russianBuilding/tetris-building.egg",
                        name = "RussianBuilding",localSize = .074, **a)
 
-def discoHall(**a):  # Looks like it's broken but it's not!  All white so you need directional light
-    return modelHandle(g.pandaPath + "/models/discohall/disco_hall", name = 'Disco Hall', **a)
+def discoHall(**a):  # Seems broken - no local size?
+    return modelHandle("discohall/disco_hall.egg", name = 'Disco Hall', **a)
 
 def grassScene(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle("models/environment", name = 'Environment', **a)
+    return modelHandle("environment.egg.pz", name = 'Environment', **a)
 
-def trainEngineScene(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/trainengine/trainengine",
-                       name = "Trainengine",
-                       localPosition = P3(.7, 0, 0),
-                       **a)
+# Not a scene and needs to be sized correctly
+#def trainEngineScene(**a):#Works as of 6-23-08 ~ Kendric
+#    return modelHandle("trainengine/trainengine.egg",
+#                       name = "Trainengine",
+#                       localPosition = P3(.7, 0, 0),
+#                       **a)
 
 def forestSky(**a):#Works as of 6-23-08 ~ Kendric - I think.  Looks cool.
-    return modelHandle(g.pandaPath + "/models/forestSky/forestsky", name = "ForestSky",
+    return modelHandle("forestSky/forestsky.egg", name = "ForestSky",
                         **a)
 
 def farmSky(**a):#Works as of 6-23-08 ~ Kendric
-    return modelHandle(g.pandaPath + "/models/farmSky/farmsky", name = "FarmSky",
+    return modelHandle("farmSky/farmsky.egg", name = "FarmSky",
                         **a)
 
 def sunset(**a):
-    return modelHandle(g.pandaPath + "/models/sunset/sunset", name = "Sunset",
+    return modelHandle("sunset/sunset.egg", name = "Sunset",
                         **a)
 
-def celestial(**a):
-    return modelHandle(g.pandaPath + "/models/celestial/celestial", name = "Celestial",
-                        localSize = .016, **a)
+#def celestial(**a):   # Seems broken?
+#    return modelHandle("celestial/celestial.egg", name = "Celestial",
+#                        localSize = .016, **a)
