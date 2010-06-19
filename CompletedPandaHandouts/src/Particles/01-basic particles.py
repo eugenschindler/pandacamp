092e5b90e206a9ae75dad8a2cbeb6491b57cc7bb
michael.reed33 <michael.reed33@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-19 17:11:57 +0000

    added myfire1.png and reorganized basic particles. added explosion vs. explosions and fireWork vs. fireWorks.

--- CompletedPandaHandouts/src/Particles/01-basic particles.py
+++ CompletedPandaHandouts/src/Particles/01-basic particles.py
@@ -3,22 +3,21 @@ from Panda import *
 #Particle effects are created much like Models.
 #Simply call the function with the name of the particle effect
 world.color = black
-#intervalRings(position = P3(0,0,0))
+
+# Run each of these and see what they do
+intervalRings(position = P3(0,0,0))
 #likeFountainWater(position = P3(0,0,0))
 #shakenSparkles(position = P3(0,0,0), color=yellow, endColor=blue)
 #warpSpeed(position = P3(0,10,0))
 #heavySnow(position = P3(0,0,0))
 #lightSnow(position = P3(0,0,0))
-#p = explosion(position = P3(0,0,0))
-#fw = fireWork(position = P3(0,0,0))
-#ex = explosion(position = P3(-1,0,0) )
-
-f = fireish(position = P3(0,0,1), texture="myfire.png")
-def shutup(m, v):
-    m.stop()
-#f.react1(localTimeIs(10), shutup)
+#explosions(position = P3(0,0,0))
+#fireWork(position = P3(0,0,0))
+#fireWorks(position = P3(0,0,0))
+#explosion(position = P3(-1,0,0) )
+#warpFace(position = P3(0,0,1), texture="face.png")
+#fireish(position = P3(0,0,1), texture="myfire.png")
 
 
-# Run each of these and see what they do
 
 start()
