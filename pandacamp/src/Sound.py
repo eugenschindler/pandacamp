4f284ad5ceaa141fc6d54550a590b5c1ca39575f
ProfessorJohnPeterson@gmail.com <ProfessorJohnPeterson@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-19 20:28:58 +0000

    Fixed search path stuff

--- pandacamp/src/Sound.py
+++ pandacamp/src/Sound.py
@@ -3,20 +3,28 @@
 
 import direct.directbase.DirectStart
 import g
+from direct.actor import Actor
+from Time import *
+from Signal import *
+from Numerics import *
 from Types import *
+from Switchers import *
+from Handle import *
+
 
 class Sound:
     def __init__(self, filePath, loopCount = 1, volume = 0.5, kill = None):
 
-        self.filePath = g.pandaPath + "/sounds/" + filePath
+        self.filePath = getSoundFile(filePath)
         self.type = SoundType
         self.volume = volume
         self.loopCount = loopCount
 
         #Why are we loading it twice? -Michael Reed s10
-        loader.loadSfx(self.filePath)
+        # loader.loadSfx(self.filePath)
         # Need to do argument type checking
         #self.loopCount
+        print self.filePath
         self.sound = loader.loadSfx(self.filePath)
         self.sound.setVolume(self.volume)
     def __str__(self):
@@ -30,3 +38,15 @@ class Sound:
 
 def sound(*p, **k):
     return Sound(*p, **k)
+
+
+def getSoundFile(file):
+    f = Filename.expandFrom(file)
+    if (f.exists()):
+        return f
+    f = g.pandaPath + "/sounds/" + file
+    if (Filename.expandFrom(f).exists()):
+        print "Loaded from library:" + f
+        return f
+    print "Sound " + file + " not found."
+    return Filename.expandFrom(g.pandaPath + "/sounds/duck.wav")
