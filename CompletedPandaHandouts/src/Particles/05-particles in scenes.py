7051722fb36dfb06448575ac8d12093f4a626e91
michael.reed33 <michael.reed33@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-19 21:01:26 +0000

    got rid of the alarm

--- CompletedPandaHandouts/src/Particles/05-particles in scenes.py
+++ CompletedPandaHandouts/src/Particles/05-particles in scenes.py
@@ -4,9 +4,6 @@ from Panda import *
 # and you can make the explosions go in different directions.
 r2d2()
 
-def boom(p, v):
-    explosion(position = P3(0,0,1), hpr = HPR(0, localTime, 0))
-
-react(alarm(start = 0, step = 1), boom)
+likeFountainWater(position = P3(0,0,1), hpr = HPR(0, localTime, 0))
 
 start()
