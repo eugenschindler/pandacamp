00ec57b9bb11d3c8f64bf2afabf70644e8f30ba8
professorjohnpeterson <ProfessorJohnPeterson@gmail.com>
2010-06-19 03:48:40 +0000

    Final version of lights

--- CompletedPandaHandouts/src/Lights/04-Directional Light.py
+++ CompletedPandaHandouts/src/Lights/04-Directional Light.py
@@ -8,12 +8,12 @@ d = discoHall(position = P3(0, 50, 0), hpr = HPR(pi/2, 0, 0))
 p = panda(position = P3(0, 60, 3), size = 3)
 
 #control RGB of point light
-PLColor = sliderColor(label="PL")
+PLColor = sliderColor(label="DL Color")
 
 dlh = slider(max = 10, min = 0, label="DL heading")
 dlp = slider(max = 10, min = 0, label="DL pitch")
 
-dl = directionallight(color = PLColor, hpr = HPR(dlh,dlp,0) )
+dl = directionalLight(color = PLColor, hpr = HPR(dlh,dlp,0) )
 
 # The disco is completely white - without directional light it's invisible!
 # Note how the directional light works - surfaces that face the same way
