a4d070400a92a83edac6cc0c177b10af6757a36f
ProfessorJohnPeterson@gmail.com <ProfessorJohnPeterson@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-22 14:00:28 +0000

    New unit

---
+++ CompletedPandaHandouts/src/Into to Python/06-randoms.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="fac_peterson"
+__date__ ="$Jun 18, 2010 2:08:21 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
