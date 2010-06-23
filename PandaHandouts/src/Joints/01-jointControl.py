7809447fb3d1bb7d0510d7edf83d11219852482f
michael.reed33 <michael.reed33@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-23 22:33:02 +0000

    fixed camel caps with lights

--- PandaHandouts/src/Joints/01-jointControl.py
+++ PandaHandouts/src/Joints/01-jointControl.py
@@ -2,20 +2,21 @@ from Panda import *
 
 camera.position = P3(0, -2, .5)
 world.color = black
-ambientlight(color = gray)
-directionallight(color = white, hpr = HPR(2,.5, 0))
+ambientLight(color = gray)
+directionalLight(color = white, hpr = HPR(2,.5, 0))
 
-h = slider(min = -pi, max = pi, label = "h")
-p = slider(min = -pi, max = pi, label = "p")
-r = slider(min = -pi, max = pi, label = "r")
+h = slider(min = 0, max = pi, label = "h")
+p = slider(min = 0, max = pi, label = "p")
+r = slider(min = 0, max = pi, label = "r")
 
 #group the three slider values together
 hpr = HPR(h, p, r)
-hpr1 = HPR(pi*time/10, 0, 1)
-hpr2 = HPR(time*1, time, 1)
-hpr3 = HPR(time, 0, time)
+hpr1 = HPR(0, 0, 0)
+hpr2 = HPR(0, 0, 0)
+hpr3 = HPR(0, 0, 0)
 
-s = sonic(hpr = HPR(pi,0,0))
+s = sonic(hpr = HPR(0,0,0))
+
 
 # Some models have joints - places where you can alter the model shape.
 # sonic has a lot of joints:
@@ -30,6 +31,21 @@ s.leftKnee = hpr
 s.rightKnee = hpr1
 s.leftHip = hpr2
 s.neck = hpr3
+
+
+h1 = slider(min = -3, max = pi, label = "h1")
+p1 = slider(min = -3, max = pi, label = "p1")
+r1 = slider(min = -3, max = pi, label = "r1")
+
+hpr = HPR(h1, p1, r1)
+hpr1 = HPR(0, 0, 0)
+hpr2 = HPR(0, 0, 0)
+hpr3 = HPR(0, 0, 0)
+
+s.rightShoulder = hpr
+s.rightElbow= hpr1
+h = s.rightWrist
+h=hpr2
 
 start()
 # Connect the right knee to the negative of this hpr - what happens?  Why?
