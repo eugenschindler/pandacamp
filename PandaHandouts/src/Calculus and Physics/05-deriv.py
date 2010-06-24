3879da411fa84f04dfdfe033a0d2506452d9500e
ProfessorJohnPeterson@gmail.com <ProfessorJohnPeterson@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2010-06-24 03:22:17 +0000

    New unit!

---
+++ PandaHandouts/src/Calculus and Physics/05-deriv.py
@@ -0,0 +1,14 @@
+
+from Panda import *
+
+p = panda()
+text(time)  # Show elapsed time
+# Maybe start without the sin(time*3)
+b = P3C(3, time, sin(time*3))
+p.position = b
+# db = deriv(P3(0,0,0), b)
+# p.hpr = P3toHPR(db)
+
+
+
+start()
