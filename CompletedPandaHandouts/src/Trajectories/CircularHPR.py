d8245b66a95c3b35511c48f30c5033abd2f5ccf8
emily.jackson <emily.jackson@western.edu>
2011-03-22 16:41:00 +0000

    fixed bad update in models.py.

--- CompletedPandaHandouts/src/Trajectories/CircularHPR.py
+++ CompletedPandaHandouts/src/Trajectories/CircularHPR.py
@@ -1,16 +1,16 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-__author__="stu885974"
-__date__ ="$Mar 2, 2011 4:01:32 PM$"
+__author__ = "stu885974"
+__date__ = "$Mar 2, 2011 4:01:32 PM$"
 
 from Panda import *
-h = slider(min = 0, max = 2*pi, label = "heading")
-p = slider(min = 0, max = 2*pi, label = "pitch")
-r = slider(min = 0, max = 2*pi, label = "roll")
+h = slider(min=0, max=2 * pi, label="heading")
+p = slider(min=0, max=2 * pi, label="pitch")
+r = slider(min=0, max=2 * pi, label="roll")
 
 
-boeing707(hpr = HPR(h*.5*time,p*.5*time,r*.5*time), size = 2,position = P3(-0, 0, 0))
+boeing707(hpr=HPR(h * .5 * time, p * .5 * time, r * .5 * time), size=2, position=P3(-0, 0, 0))
 
 
 
