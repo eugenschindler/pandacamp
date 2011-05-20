5df1b85b772306732b008bb12e53b46d651d94a9
eric.peterson@western.edu <eric.peterson@western.edu@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-05-20 21:40:46 +0000

    Worked more on Maze.py

--- pandacamp/src/Maze.py
+++ pandacamp/src/Maze.py
@@ -1,21 +1,30 @@
 from cgi import length
 class Maze:
     def __init__(self, txt):
-        
+
+        #read the txt and store it's size
         fileLoader = open(txt,  "r")
         contents = fileLoader.read().split("\n")
-        x = 0
-        y = 0
         h = contents.length
         w = contents[0].length
 
+        bools = []
         chars = []
-        #expand char array
+        objects = []
+
+        #expand arrays
         for i in xrange(h):
             chars.append([])
+            objects.append([])
+            bools.append([])
             for j in xrange(w):
                    chars[i].append(i+j)
-        #populate  char array           
+                   objects[i].append(i+j)
+                   bools[i].append(i+j)
+
+        #populate  char array
+        x = 0
+        y = 0
         for l in contents:
             x = 0
             for c in l:
@@ -23,19 +32,23 @@ class Maze:
                 x = x + 1
             y = y + 1
         
-        
-        for i in range(15):
-            for j in range(15):
-                if a[i][j] == "x":
-                    cube(
-                    color(0, random01(),random01()),
-                    color(0, random01(),random01()),
-                    color(0, random01(),random01()),
-                    color(0, random01(),random01()),
-                    color(0, random01(),random01()),
-                    color(0, random01(),random01()),
-                    position = P3(i,j,0),size=.5)
-                if a[i][j] == "b":
-                     bunny(position = P3(i,j,0),size=.5)
-                if a[i][j] == "j":
-                     jeep(position = P3(i,j,0),size=.5)
+       #populate bool and object arrays
+        for i in range(h):
+            for j in range(w):
+                objects[i][j] = "derrr" # Run the open-'chars[h][j]:
+                bools[i][j]= char[h][j].isupper()
+
+                    
+    def find(self, c):
+        res = []
+        for i in range(h):
+            for j in range(w):
+                if (chars[i][j]==c):
+                    res.append(objects[i][j])
+        return res
+
+    def collide(self, p):
+        return bools[int(p.x)][int(p.y)]
+    
+    def get(self,p):
+        return objects[int(p.x)][int(p.y)]
