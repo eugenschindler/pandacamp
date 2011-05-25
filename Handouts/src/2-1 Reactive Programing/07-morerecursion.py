e6b613f958edef884a768f2191bf7fb00b6ccae9
professorjohnpeterson <ProfessorJohnPeterson@gmail.com>
2011-05-25 23:24:12 +0000

    Moving content over - motion and basic programming

---
+++ Handouts/src/2-1 Reactive Programing/07-morerecursion.py
@@ -0,0 +1,27 @@
+from Panda import *
+
+# We can generalize this idea a little.
+# This time, write a pandaLine function that takes three arguments:
+#  The number of pandas
+#  The location of the next panda
+#  The distance between pandas
+def pandaLine(number, loc, dist):
+    if number > 0:
+        panda(position = loc)
+        pandaLine(number-1, loc+dist, dist)
+# You need to make a panda at the right place and figure out how
+# number, loc, and dist will change in the recursion
+        
+
+
+# Make a line of pandas starting at (0,0,0) moving one unit to the right
+
+pandaLine(5, P3(0,0,0), P3(1,0,0))
+# Make another line starting at (0,0,0) moving 2 back
+
+# This time, use mouseControl to allow the camera to be moved around
+mouseControlCamera(camera)
+
+# Can you complete the square of pandas?
+# Can you use any model you want instead of just pandas?
+start()
