0bf680a838458ba59bc57adfb2b6059c260a917f
christopher.hill@western.edu <christopher.hill@western.edu@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-05-27 17:47:05 +0000

    adding PandaBlast.py

---
+++ Handouts/src/1-3 Picture Blasting/PandaBlast.py
@@ -0,0 +1,13 @@
+from Panda import *
+
+world.color = black
+#Use the slicePicture to break up a picture by rows and columns.
+(center, r) = slicePicture( "realpanda.jpg", 1, 10,  size = 2)
+#Define blast to move pieces in randam directions.
+def blast(p, v):
+    p.position = p.position.now() + integral(P3(random11()*getX(p.location), random11(), random11()*getZ(p.location)))
+    p.hpr = integral(HPR(random01()*.9, random01() * .9, 0))
+#Write a for loop to blast each piece of the picture.
+for p in r:
+    p.react1(localTimeIs(2 + getX(p.location)), blast)
+start()
