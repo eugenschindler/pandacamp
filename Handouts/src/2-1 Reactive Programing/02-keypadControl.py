cb7e0585b38d40ff3bd9e6bb46bf5aa7e3e9366d
Michael.Reed33@gmail.com <Michael.Reed33@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-05-27 21:54:22 +0000

    made it so each picture strip can move independently. Added the file skeletons for reactive Programing. added notes for guide lines.

---
+++ Handouts/src/2-1 Reactive Programing/02-keypadControl.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="stu439424"
+__date__ ="$May 27, 2011 1:58:24 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
