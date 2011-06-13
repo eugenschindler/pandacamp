c99bb3bfbfb2f19e7b50f93af102078bc053e8be
christopher.hill@western.edu <christopher.hill@western.edu@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-06-13 21:43:52 +0000

    Texturing_Chris_6/13

---
+++ Handouts/src/1-4 Texturing/02-Customizing.py
@@ -0,0 +1,10 @@
+from Panda import *
+# Take the panda texture in pictures/panda.jpg and edit it to include
+# some text or a pictures.  You can do this with any model - not just the panda!
+# Change the file names of pandaInvert and pandaW to your own panda skins.
+# Use Gimp to create a negative and add text to the other.
+# Create three pandas using the original, invert and text textures.
+
+
+
+start()
