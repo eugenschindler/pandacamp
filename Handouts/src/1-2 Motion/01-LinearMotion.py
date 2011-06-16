7ea4fa60108eaa904a137c34c2ef30915f8144c8
christopher.hill <christopher.hill@western.edu>
2011-06-16 20:11:53 +0000

    motion

--- Handouts/src/1-2 Motion/01-LinearMotion.py
+++ Handouts/src/1-2 Motion/01-LinearMotion.py
@@ -1,21 +1,25 @@
 from Panda import *
 
-p0 = P3(0,0,0)
-v = P3(3,0,0)
-
 # The integral function turns a speed into a position relative to time.  For 
 # example, if you are in a car traveling 60 mph along a straight road, after one 
 # minute you will be at the first mile marker. And so when you get to an hour
 # you will be at mile 60.  We use the integral function so the computer will 
 # know where to display the panda as time passes.
 
-# Try this: change the numbers in the v to gain a better
+# Try this: change the numbers in the velecity to gain a better
 # understanding of how it reacts with integral.  Don't forget to try decimals.
 
+location = P3(0,0,0)
+velocity = P3(0,0,0)
+pos = location + integral(velocity)
 
-pos = p0 + integral(v)
+# The Spin of an object is determind by the head, pitch, and roll or hpr.
+# The direction that the object faces can be changed with the number of
+# the hpr(note that hpr is in the increment of pi). To get a object to spin add
+# a variable of time to the hpr.
 
-p = panda(position = pos)
+spin = HPR(0,0,0)
 
+panda(position = pos, hpr = spin)
 
 start()
