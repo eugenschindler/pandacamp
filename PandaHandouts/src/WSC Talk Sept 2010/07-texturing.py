77338e00b7841043b17347d625e7c89838110545
ProfessorJohnPeterson@gmail.com <ProfessorJohnPeterson@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-06-17 03:57:30 +0000

    Moved to new handouts

--- PandaHandouts/src/WSC Talk Sept 2010/07-texturing.py
+++
@@ -1,9 +0,0 @@
-from Panda import *
-
-
-p1 = panda(position = P3(-2, 0, 0), hpr = HPR(time, 0, time/10))
-p2 = panda(position = P3(0, 0, 0), hpr = HPR(time, 0, time/10), texture = "pandaInvert.jpg")
-p3 = panda(position = P3(2, 0, 0), hpr = HPR(time, 0, time/10), texture = "pandaW.jpg")
-
-
-start()
