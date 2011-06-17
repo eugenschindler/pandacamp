728574af932259266660b2ad870cc11eeda1ff07
Michael.Reed33@gmail.com <Michael.Reed33@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-06-17 07:15:28 +0000

    Created Solid_Model.py to show an example of a solid model. Shooting Gallery now has explosions!

--- CompletedHandouts/src/3-5 Shooting Gallery Game/Shooting Gallery.py
+++ CompletedHandouts/src/3-5 Shooting Gallery Game/Shooting Gallery.py
@@ -1,5 +1,4 @@
 from Panda import *
-
 camera.position=P3(0,-50, 6)
 
 
@@ -8,6 +7,7 @@ balls = collection()
 targets = collection()
 targets.add(panda(position = P3(5,0,6),size = 2))
 targets.add(panda(position = P3(-5,0,6), size = 2))
+targets.add(panda(position = P3(sin(time/2) * 5, 5, (cos(time/2) * 5) + 6), size = 2))
 
 # grassScene(position = P3(0,10,0), scale = 0.1)
 
@@ -15,16 +15,17 @@ targets.add(panda(position = P3(-5,0,6), size = 2))
 
 gravity = P3(0,0,-25)
 
-floor = 3
+floor = 0
 wallRight = 10
 wallLeft = -10
+wallDepth = 50
 
 ### Sets up the rectangles for reference.
-wallDepth = 20
+
 br = 1 #Ball Radius
-lowerLeft = P3(wallLeft-br,-wallDepth,floor-br)
-lowerRight = P3(wallRight+br,-wallDepth,floor-br)
-backLeft = P3(wallLeft-br,wallDepth,floor-br)
+lowerLeft = P3(wallLeft-br,-wallDepth,0)
+lowerRight = P3(wallRight+br,-wallDepth,0)
+backLeft = P3(wallLeft-br,wallDepth,0)
 
 floorRect = rectangle(lowerLeft,lowerRight,backLeft,blue)
 
@@ -35,32 +36,29 @@ def launch(b, p0, v0):
     b.velocity = v0 + integral(gravity)
     b.position = p0 + integral(b.velocity)
 
-def reflectFloor(b,v):
-   p1 = b.position.now()
-   v1 = b.velocity.now()
-   p0 = P3(p1.x, p1.y, 2*floor-p1.z)
-   v0 = P3(v1.x, v1.y, -.94*v1.z)
-   launch(b, p0, v0)
-
 def newBall(m, v):
     xdir = 10*getX(mouse.now())
     ydir = 10*getY(mouse.now())+15
     b = soccerBall(size = .6)
+    b.when(getZ(b.position) < 1, disapear)
     balls.add(b)
     launch(b, P3(0,-40,4), P3(xdir,40, ydir))
 
 def h(m,e):
-    (b,t) = e
-    b.exit()
-    t.exit()
+    for p in e:
+        (b,t) = p  
+        lastFor(2, explosions(position=b.position.now()))
+        lastFor(2, explosions(position=t.position.now()))
+        b.exit()
+        t.exit()
+        
 
 def disapear(b,v):
+    explosion(position=b.position.now())
     balls.remove(b)
-    print "Bang!"
     b.exit()
 
 react(lbp, newBall)
-#react(hit(balls, targets),h)
-
+react(hit(balls, targets),h)
 
 start()
