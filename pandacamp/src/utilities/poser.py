66417c7cd795aecd496eec66ab2a0b0fb1c5cd84
professorjohnpeterson <ProfessorJohnPeterson@gmail.com>
2011-06-18 15:06:20 +0000

    Changed default back to sonic

--- pandacamp/src/utilities/poser.py
+++ pandacamp/src/utilities/poser.py
@@ -10,7 +10,7 @@ import g
 ##############################
 #fileName = 'c:/pose.csv'
 fileName = g.pandaPath + '/Scripts/sonic.csv'
-model = bender()
+model = sonic()
 ##############################
 
 # DLight(HPR = HPR(time, .3,  0))
