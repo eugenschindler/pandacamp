a1dbeca328e415f6a690eefe9a14751a3125c693
david.adair@western.edu <david.adair@western.edu@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-06-19 20:14:55 +0000

    fixed up model in jointcontrol and interpolation changed joints and added a new poseDance file

--- Handouts/src/3-4 Joints/01-jointControl.py
+++ Handouts/src/3-4 Joints/01-jointControl.py
@@ -1,3 +1,4 @@
+
 from Panda import *
 
 camera.position = P3(0, -2, .5)
@@ -5,17 +6,11 @@ world.color = black
 ambientLight(color = gray)
 directionalLight(color = white, hpr = HPR(2,.5, 0))
 
-h = slider(min = 0, max = pi, label = "h")
-p = slider(min = 0, max = pi, label = "p")
-r = slider(min = 0, max = pi, label = "r")
+hpr = sliderHPR()
 
 #group the three slider values together
-hpr = HPR(h, p, r)
-hpr1 = HPR(0, 0, 0)
-hpr2 = HPR(0, 0, 0)
-hpr3 = HPR(0, 0, 0)
 
-s = sonic(hpr = HPR(0,0,0))
+s = sonic()
 
 
 # Some models have joints - places where you can alter the model shape.
@@ -27,25 +22,16 @@ s = sonic(hpr = HPR(0,0,0))
 #  leftKnee, rightKnee, leftAnkle, rightAnkle
 
 #sets the model's left knee's hpr to the hpr specified.
-s.leftKnee = hpr
-s.rightKnee = hpr1
-s.leftHip = hpr2
-s.neck = hpr3
+s.leftEyeBrow = HPR(0,3.141592741,0)
+s.leftElbow = HPR(-3.141592741,-0.606272578,1.929047227)
+s.leftShoulder = HPR(-0.275579333,-0.771620095,-0.110231653)
+s.rightElbow = HPR(-0.385809273,3.141592741,-1.157429338)
+s.rightShoulder = HPR(0,3.141592741,0)
+
+s.neck = hpr
 
 
-h1 = slider(min = -3, max = pi, label = "h1")
-p1 = slider(min = -3, max = pi, label = "p1")
-r1 = slider(min = -3, max = pi, label = "r1")
 
-hpr = HPR(h1, p1, r1)
-hpr1 = HPR(0, 0, 0)
-hpr2 = HPR(0, 0, 0)
-hpr3 = HPR(0, 0, 0)
-
-s.rightShoulder = hpr
-s.rightElbow= hpr1
-h = s.rightWrist
-h=hpr2
 
 start()
 # Connect the right knee to the negative of this hpr - what happens?  Why?
