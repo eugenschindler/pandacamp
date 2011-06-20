dd50aff078265b0d905cf45a13801b747e91d103
ProfessorJohnPeterson@gmail.com <ProfessorJohnPeterson@gmail.com@0329e92e-1c46-11df-8da0-9131547f57cf>
2011-06-20 03:00:08 +0000

    Done!

--- CompletedHandouts/src/3-3 Math with dr Bob/rolling.py
+++ CompletedHandouts/src/3-3 Math with dr Bob/rolling.py
@@ -1,40 +1,17 @@
 from Panda import *
 
 def f(x,y):
-    if x < 0:
-        return 0
-    else:
-        return x
+    return (x*x + y*y)/10
 
 s = surface(f, xmin = -1, ymin = -1, xmax = 1, ymax = 1, texture = "realpanda.jpg")
 
 b = soccerBall(size = .1)
+rollSphere(b, s, .5, .5, 1, 0)
 
-g = -9.8
-x0 = .5
-y0 = .5
-xv0 = 1
-yv0 = 0
-drag = .3
-setType(b.xpos, numType)
-setType(b.ypos, numType)
-setType(b.xv, numType)
-setType(b.yv, numType)
-setType(b.xa, numType)
-setType(b.ya, numType)
-dx = s.dx(b.xpos, b.ypos)
-dy = s.dy(b.xpos, b.ypos)
-den = (1 + dx*dx + dy*dy)
-b.xa = g * dx/den - drag*b.xv
-b.ya = g * dy/den - drag*b.yv
-b.xv = integral(b.xa) + xv0    
-b.yv = integral(b.ya) + yv0
-b.xpos = integral(b.xv) + x0    
-b.ypos = integral(b.yv) + y0
-
-
-p = P3(b.xpos, b.ypos, s.f(b.xpos, b.ypos))
-b.position = p
+j = sphere(texture = "jupitermap.jpg", size = .2)
+rollSphere(j, s, -.5, .5, -1, 0)
 
 mouseControlCamera(camera)
+
+
 start()
