09ba7c6876cd0be6292a1f816b5fb78af6ad7eea
professorjohnpeterson <ProfessorJohnPeterson@gmail.com>
2011-06-24 03:37:08 +0000

    Bouncing works

--- CompletedHandouts/src/3-3 Math with dr Bob/rolling4.py
+++ CompletedHandouts/src/3-3 Math with dr Bob/rolling4.py
@@ -1,22 +1,20 @@
 from Panda import *
 
 ambientLight(color = color(.5, .5, .5))
-directionalLight(color = color(0.6, 0.6, 0.6), hpr = sliderHPR())
+directionalLight(color = color(0.6, 0.6, 0.6), hpr = HPR(5,5,0))
 def f(x,y):
     return (smoothStep(5-x))/2.0
 
 
-b = soccerBall(size = .2)
-
-
-s = golfCourse(10, 10, f, b)
+s = golfCourse(f, 10, 10, -8, .2)
 
 j = sphere(texture = "jupitermap.jpg", size = .2)
-rollSphere(j, s, 0, 0, 0, 0)
-rollSphere(b, s, 2, 2, -1, -1)
+rollSphere(j, s, .5, .5, 10, 10)
+addWallBounces(s, j)
 
 
 
-mouseControlCamera(camera)
-
+#mouseControlCamera(camera)
+camera.position = P3(5, -10, 6)
+camera.hpr = HPR(0,6,0)
 start()
